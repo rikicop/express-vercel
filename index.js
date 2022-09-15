40080e88941dfe629d6191eb4c094ccb65e7b7bb
rikicop <ruperto1@protonmail.com>
2022-09-15 13:08:38 -0500

    hola3

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const express = require("express");
 const app = express();
 //const product = require("./api/product");
 
+require('dotenv').config()
+
 app.use(express.json({ extended: false }));
 
-app.get('/api/insectos', (req, res) => { res.status(200).json({ message: 'Insectos' }) })
+app.get('/api/insectos', (req, res) => { res.status(200).json({ message: `App corre en puerto ${PORT}` }) })
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
