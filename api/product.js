53dc58e93bc30167c5cb6433ddcff9fe58bc17de
rikicop <ruperto1@protonmail.com>
2022-09-15 13:02:39 -0500

    hola

--- api/product.js
+++ api/product.js
@@ -10,7 +10,7 @@ router.get("/", async (req, res) => {
   try {
     res.json({
       status: 200,
-      message: "Get data has successfully",
+      message: "Corre exitosamente",
     });
   } catch (error) {
     console.error(error);
