10ef5445a8a80fc732d64ae8c2c92f3e056fde0c
rikicop <ruperto1@protonmail.com>
2022-09-15 13:04:52 -0500

    hola2

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const app = express();
-const product = require("./api/product");
+//const product = require("./api/product");
 
 app.use(express.json({ extended: false }));
 
-app.use("/api/product", product);
+app.get('/api/insectos', (req, res) => { res.status(200).json({ message: 'Insectos' }) })
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
